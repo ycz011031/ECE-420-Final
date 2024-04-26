e9a2fe0582e88f6527f13ee04c591f6500dc7c1b
unknown <yz69@illinois.edu>
2024-04-26 00:10:46 -0500

    Android Build Audio Record cmt 1

--- Android_Build/app/src/main/java/com/manikbora/multiscreenapp/ThirdActivity.java
+++ Android_Build/app/src/main/java/com/manikbora/multiscreenapp/ThirdActivity.java
@@ -13,6 +13,28 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+
+
 public class ThirdActivity extends AppCompatActivity {
 
     String[] item = {"Set fire to the rain", "pre-record 2", "Live Record"};
@@ -20,6 +42,17 @@ public class ThirdActivity extends AppCompatActivity {
     AutoCompleteTextView autoCompleteTextView;
     ArrayAdapter<String> adapterItems;
 
+    private MediaRecorder mRecorder = null;
+
+    private MediaPlayer mPlayer;
+
+    private static String mFileName = null;
+
+    public static final int REQUEST_AUDIO_PERMISSION_CODE =1;
+
+    boolean is_recording = false;
+
+    boolean is_playing_usr = false;
 
 
 
@@ -67,8 +100,148 @@ public class ThirdActivity extends AppCompatActivity {
             }
         });
 
+        Button btnrecord = findViewById(R.id.btn_record);
+        btnrecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!is_recording){
+                    startRecording();
+                }
+                else{
+                    pauseRecording();
+                }
+                is_recording = !is_recording;
+            }
+        });
+
+        Button btnplay = findViewById(R.id.btn_play_trg);
+        btnplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!is_playing_usr){
+                    playAudio();
+                }
+                else{
+                    pausePlaying();
+                }
+                is_playing_usr = !is_playing_usr;
+            }
+        });
 
 
 
     }
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // this method is called when user will
+        // grant the permission for audio recording.
+        switch (requestCode) {
+            case REQUEST_AUDIO_PERMISSION_CODE:
+                if (grantResults.length > 0) {
+                    boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (permissionToRecord && permissionToStore) {
+                        Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    public boolean CheckPermissions() {
+        // this method is used to check permission
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void RequestPermissions() {
+        // this method is used to request the
+        // permission for audio recording and storage.
+        ActivityCompat.requestPermissions(ThirdActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
+    private void startRecording() {
+        if (CheckPermissions()) {
+            mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFileName += "/AudioRecording.3gp";
+
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            mRecorder.setOutputFile(mFileName);
+            try {
+                // below method will prepare
+                // our audio recorder class
+                mRecorder.prepare();
+            } catch (IOException e) {
+                Log.e("TAG", "prepare() failed");
+            }
+            // start method will start
+            // the audio recording.
+            mRecorder.start();
+        }
+        else {
+            // if audio recording permissions are
+            // not granted by user below method will
+            // ask for runtime permission for mic and storage.
+            RequestPermissions();
+
+        }
+    }
+
+    public void pauseRecording() {
+
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+
+    }
+
+    public void pausePlaying() {
+        // this method will release the media player
+        // class and pause the playing of our recorded audio.
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    public void playAudio() {
+        // for playing our recorded audio
+        // we are using media player class.
+        mPlayer = new MediaPlayer();
+        try {
+            // below method is used to set the
+            // data source which will be our file name
+            mPlayer.setDataSource(mFileName);
+
+            // below method will prepare our media player
+            mPlayer.prepare();
+
+            // below method will start our media player.
+            mPlayer.start();
+
+        } catch (IOException e) {
+            Log.e("TAG", "prepare() failed");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 }
