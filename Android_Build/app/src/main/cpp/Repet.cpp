ec92a451007f04d7075af7c741a19fafdbdeb403
unknown <yz69@illinois.edu>
2024-04-28 12:55:58 -0500

    Android Build, cpp processing frame work complete

--- Android_Build/app/src/main/cpp/Repet.cpp
+++ Android_Build/app/src/main/cpp/Repet.cpp
@@ -3,8 +3,20 @@
 //
 
 #include <jni.h>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
 
 #include "kiss_fft/kiss_fft.h"
+
+
+void processAudio(int* input, int* output1, int* output2, int length){
+
+    for (int i=0; i<length; i++){
+        output1[i] = input[i];
+        output2[length-i-1]  = input[i];
+    }
+}
 
 extern "C" JNIEXPORT void JNICALL Java_com_manikbora_multiscreenapp_SecondActivity_repet(
         JNIEnv* env, jobject obj, jintArray inputArray, jintArray outputArray1, jintArray outputArray2) {
@@ -12,24 +24,21 @@ extern "C" JNIEXPORT void JNICALL Java_com_manikbora_multiscreenapp_SecondActivi
     // Get the length of the input array
     jsize length = env->GetArrayLength(inputArray);
 
-    // Get the elements of the input array
-    jint* input = env->GetIntArrayElements(inputArray, nullptr);
-
     // Allocate memory for output arrays using JNI
-    jint* output1 = new jint[length];
-    jint* output2 = new jint[length];
+
+
+    jint* input = env->GetIntArrayElements(inputArray,nullptr);
+    jint* output1 = env->GetIntArrayElements(outputArray1,nullptr);
+    jint* output2 = env->GetIntArrayElements(outputArray2,nullptr);
+
 
     // Example processing: copy input to both outputs
-    memcpy(output1, input, length * sizeof(jint));
-    memcpy(output2, input, length * sizeof(jint));
-
-    // Set the result back to Java array objects
-    env->SetIntArrayRegion(outputArray1, 0, length, output1);
-    env->SetIntArrayRegion(outputArray2, 0, length, output2);
+    processAudio(input,output1,output2,length);
 
     // Release memory and handle JNI clean-up
     env->ReleaseIntArrayElements(inputArray, input, 0); // Release without copying back changes
-    delete[] output1;
-    delete[] output2;
+    env->ReleaseIntArrayElements(outputArray1,output1,0);
+    env->ReleaseIntArrayElements(outputArray2,output2,0);
 }
+
 
